18c7c0ee3330fa6ffe51755d3e4dead8b7b22a89
ultimatekristency <ultimatekristency@gmail.com>
2025-09-30 09:45:44 +0100

    first commit

--- target/boot/debug/boot.d
+++
@@ -1,1 +0,0 @@
-/home/kristency/Projects/polyglot-os/target/boot/debug/boot: /home/kristency/Projects/polyglot-os/boot/src/main.rs
--- target/kernel/debug/libkernel.d
+++
@@ -1,1 +0,0 @@
-/home/kristency/Projects/polyglot-os/target/kernel/debug/libkernel.rlib: /home/kristency/Projects/polyglot-os/kernel/src/lib.rs
